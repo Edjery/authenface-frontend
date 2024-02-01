df678ab1f8dc8211e195edd7c9929ebe4e9b61cb
Edjery Gumbao <edisgumbao@gmail.com>
2024-02-01 13:24:05 +0800

    added theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,29 @@ import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <ChakraProvider>
-      <Grid
-        templateAreas={`"header header" "nav main" "nav footer"`}
-        gridTemplateRows={"50px 1fr 30px"}
-        gridTemplateColumns={"150px 1fr"}
-        h="800px"
-        gap="1"
-        color="blackAlpha.700"
-        fontWeight="bold"
-      >
-        <GridItem pl="2" area={"header"}>
-          <NavBar />
-        </GridItem>
-        <GridItem pl="2" bg="pink.300" area={"nav"}>
-          Nav
-        </GridItem>
-        <GridItem pl="2" bg="green.300" area={"main"}>
-          Main
-          <Box>Hello World</Box>
-        </GridItem>
-        <GridItem pl="2" bg="blue.300" area={"footer"}>
-          Footer
-        </GridItem>
-      </Grid>
-    </ChakraProvider>
+    <Grid
+      templateAreas={`"header header" "nav main" "nav footer"`}
+      gridTemplateRows={"50px 1fr 30px"}
+      gridTemplateColumns={"150px 1fr"}
+      h="800px"
+      gap="1"
+      color="blackAlpha.700"
+      fontWeight="bold"
+    >
+      <GridItem pl="2" area={"header"}>
+        <NavBar />
+      </GridItem>
+      <GridItem pl="2" bg="pink.300" area={"nav"}>
+        Nav
+      </GridItem>
+      <GridItem pl="2" bg="green.300" area={"main"}>
+        Main
+        <Box>Hello World</Box>
+      </GridItem>
+      <GridItem pl="2" bg="blue.300" area={"footer"}>
+        Footer
+      </GridItem>
+    </Grid>
   );
 }
 
