8322cebc9adf84a964c1b3710162b64ee37c0d44
EJ <edisgumbao@gmail.com>
2024-02-21 10:39:05 +0800

    added auths

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,7 +1,12 @@
 import { Box, Heading } from "@chakra-ui/react";
+import { Navigate } from "react-router-dom";
 import Snapshots from "../components/Snapshots";
+import useAuth from "../hooks/useAuth";
 
 const Gallery = () => {
+  const { user } = useAuth();
+  if (!user) return <Navigate to="/login" />;
+
   return (
     <Box mx={5} my={10}>
       <Heading>Snapshots</Heading>
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,6 +1,11 @@
 import { Box, Heading } from "@chakra-ui/react";
+import { Navigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 const Profile = () => {
+  const { user } = useAuth();
+  if (!user) return <Navigate to="/login" />;
+
   return (
     <Box mx={5} my={10}>
       <Heading>Profile</Heading>
