2a86148d9171c42ece3e21d4a13b308ec37de8b4
EJ <edisgumbao@gmail.com>
2024-02-21 10:17:42 +0800

    add home url in navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,13 @@
 import { HStack, Heading } from "@chakra-ui/react";
 import ColorModeSwitch from "./ColorModeSwitch";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <HStack mt={5} mx={10} justifyContent="space-between">
-      <Heading>AuthenFace</Heading>
+      <Link to="/">
+        <Heading>AuthenFace</Heading>
+      </Link>
       <ColorModeSwitch />
     </HStack>
   );
