76695c29cc98d50024659274df71e42b66ce0659
EJ <edisgumbao@gmail.com>
2024-02-21 09:02:03 +0800

    add link on sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,11 +1,12 @@
-import { Link, VStack } from "@chakra-ui/react";
+import { VStack } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const SideBar = () => {
   return (
     <VStack fontSize="xl" as="b" mt={10}>
-      <Link>Home</Link>
-      <Link>Snapshots</Link>
-      <Link>Profile</Link>
+      <Link to="/">Home</Link>
+      <Link to="/gallery">Gallery</Link>
+      <Link to="/profile">Profile</Link>
     </VStack>
   );
 };
