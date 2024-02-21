2354a4cad5c9c8735258d78fcf3b1b720d170e94
EJ <edisgumbao@gmail.com>
2024-02-21 10:17:22 +0800

    add auth

---
+++ src/pages/auth/AuthLayout.tsx
@@ -0,0 +1,36 @@
+import { Grid, GridItem, useColorMode } from "@chakra-ui/react";
+import { Outlet } from "react-router-dom";
+import NavBar from "../../components/NavBar";
+import SideBar from "../../components/SideBar";
+
+const AuthLayout = () => {
+  const { colorMode } = useColorMode();
+
+  return (
+    <>
+      <Grid
+        templateAreas={`"header header header" "leftSide main rightSide"`}
+        gridTemplateRows={"90px 1fr"}
+        gridTemplateColumns={"500px 1fr 500px"}
+        rowGap="10"
+      >
+        <GridItem
+          pl="2"
+          area={"header"}
+          bg={colorMode === "dark" ? "blue.900" : "blue.200"}
+        >
+          <NavBar />
+        </GridItem>
+        <GridItem
+          pl="2"
+          area={"main"}
+          bg={colorMode === "dark" ? "blue.800" : "blue.100"}
+        >
+          <Outlet />
+        </GridItem>
+      </Grid>
+    </>
+  );
+};
+
+export default AuthLayout;
