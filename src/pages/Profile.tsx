a144f0640806114d9318eda48e777f8025af8b76
EJ <edisgumbao@gmail.com>
2024-02-21 10:16:38 +0800

    added profile and other routes

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,10 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+const Profile = () => {
+  return (
+    <Box mx={5} my={10}>
+      <Heading>Profile</Heading>
+    </Box>
+  );
+};
+export default Profile;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,18 +1,34 @@
 import { createBrowserRouter } from "react-router-dom";
-import HomePage from "./pages/HomePage";
 import Gallery from "./pages/Gallery";
+import HomePage from "./pages/HomePage";
 import Layout from "./pages/Layout";
+import Profile from "./pages/Profile";
+import Login from "./pages/auth/login";
+import AuthLayout from "./pages/auth/AuthLayout";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [{ index: true, element: <HomePage /> }],
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "/login", element: <Login /> },
+    ],
   },
   {
     path: "/gallery",
     element: <Layout />,
     children: [{ index: true, element: <Gallery /> }],
+  },
+  {
+    path: "/profile",
+    element: <Layout />,
+    children: [{ index: true, element: <Profile /> }],
+  },
+  {
+    path: "/login",
+    element: <AuthLayout />,
+    children: [{ index: true, element: <Login /> }],
   },
 ]);
 
