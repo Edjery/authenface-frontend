fd389429d95b8185d3cf9a7eafd684c637f621f8
EJ <edisgumbao@gmail.com>
2024-03-06 16:09:04 +0800

    adjust timer close on register

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -75,7 +75,7 @@ class SnapshotService {
                 .toUpperCase()}: ${error.response.data[key].toString()}`;
               toast.error(errorMessage, {
                 position: "bottom-center",
-                autoClose: 5000,
+                autoClose: 3000,
                 hideProgressBar: false,
                 closeOnClick: true,
                 pauseOnHover: true,
