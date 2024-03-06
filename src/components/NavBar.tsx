8bc5748f97e8f6ae72551815eb4277a6741a759f
EJ <edisgumbao@gmail.com>
2024-03-06 15:25:11 +0800

    Move logout button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,33 @@
-import { HStack, Heading } from "@chakra-ui/react";
+import { Box, Button, HStack, Heading } from "@chakra-ui/react";
+import useAuthHeader from "react-auth-kit/hooks/useAuthHeader";
+import { Link, useNavigate } from "react-router-dom";
 import ColorModeSwitch from "./ColorModeSwitch";
-import { Link } from "react-router-dom";
+import useSignOut from "react-auth-kit/hooks/useSignOut";
+import useIsAuthenticated from "react-auth-kit/hooks/useIsAuthenticated";
 
 const NavBar = () => {
+  const isAuthenticated = useIsAuthenticated();
+  const signOut = useSignOut();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    signOut();
+    navigate("/login");
+  };
+
   return (
     <HStack mt={5} mx={10} justifyContent="space-between">
       <Link to="/">
         <Heading>AuthenFace</Heading>
       </Link>
-      <ColorModeSwitch />
+      <HStack>
+        <ColorModeSwitch />
+        {isAuthenticated() && (
+          <Button onClick={handleLogout} colorScheme="red" mx={10}>
+            Logout
+          </Button>
+        )}
+      </HStack>
     </HStack>
   );
 };
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,11 +1,10 @@
-import { Box, Button, HStack, Heading } from "@chakra-ui/react";
+import { Box, HStack, Heading } from "@chakra-ui/react";
 
 const Profile = () => {
   return (
     <Box mx={5} my={10}>
       <HStack justify="space-between">
         <Heading>Profile</Heading>
-        <Button colorScheme="red">Logout</Button>
       </HStack>
     </Box>
   );
