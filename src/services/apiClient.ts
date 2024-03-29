c140d0f52c50778aa2f492733f7c028b308d813a
EJ <edisgumbao@gmail.com>
2024-03-29 10:15:54 +0800

    add host compatibility

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8000/api";
+// const BASE_URL = "192.168.1.169";
+const BASE_URL = "localhost";
+const API_BASE_URL = `http://${BASE_URL}:8000/api`;
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
 });
